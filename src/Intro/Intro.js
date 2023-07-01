87dfd8f17d61d0437cccca32c24c3d215246bde8
Jeevan <jeevankumarhari2002@gmail.com>
2023-07-01 09:02:13 +0530

    Refactored text alignment.

--- src/Intro/Intro.js
+++ src/Intro/Intro.js
@@ -4,7 +4,7 @@ const Intro = () =>
     <div className='intro'>
         <p className='lcm'>LIFE CYCLE MANAGEMENT</p>
         <h1 className='heading' style={{ fontSize:"40px"}}>Streamline, Secure, Succeed: Redefine Your <br/>Enterprise's Apple Experience With Our Device <br/>Lifecycle Management Service</h1>
-        <p className='info'>Welcome to a new era of enterprise excellence. With our comprehensive Apple Device Lifecycle Management <br/> Service, you can revolutionize the way your business operates. Seamlessly integrating technology, security, and <br/> efficiency, we empower enterprises to reach new heights of productivity and success. From the moment you <br/> procure your Apple devices to their retirement, we are your trusted partner, ensuring seamless device management,<br/> uncompromising data security, and unparalleled peace of mind. Elevate your enterprise today and experience a <br/> future where efficiency and security go hand in hand.</p>
+        <p className='lcmInfo'>Welcome to a new era of enterprise excellence. With our comprehensive Apple Device Lifecycle Management <br/> Service, you can revolutionize the way your business operates. Seamlessly integrating technology, security, and <br/> efficiency, we empower enterprises to reach new heights of productivity and success. From the moment you <br/> procure your Apple devices to their retirement, we are your trusted partner, ensuring seamless device management,<br/> uncompromising data security, and unparalleled peace of mind. Elevate your enterprise today and experience a <br/> future where efficiency and security go hand in hand.</p>
         <div className='bigCircle'/>
     </div>
 
